217a2407700f31aa11c5003f77d7f783355ecce3
breinbaas | rob van putten <breinbaasnl@gmail.com>
2020-09-24 14:25:29 +0200

    Working prototype

--- tests/test_dstability.py
+++ tests/test_dstability.py
@@ -2,10 +2,9 @@ from mlas_dseries.dstability import DStability
 from mlas.models.calculationmodel import CalculationModel
 
 def test_dstability():
-    calculationmodel = CalculationModel.parse("./testdata/crosssections/A145_2300.json")
+    calculationmodel = CalculationModel.parse("./testdata/crosssections/crosssection_0.json")
     dstability = DStability(calculationmodel=calculationmodel)
     dstability.to_file(save_path="./testdata/output", save_name="crosssection_0")
-
 
 if __name__=="__main__":
     test_dstability()
